41d22639703b1acf458a722c542b257dbd79f12b
Vlod-and-whot <cherkunovvv@gmail.com>
2025-03-18 19:20:15 +0200

    ы

---
+++ sample/mailCook.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import session
+import datetime
+import requests
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    elif visits_count == 5:
+        res = make_response(
+            f"Вы пришли на эту страницу 5 раз")
+        res.set_cookie("visits_count", str(500), max_age=0)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
---
+++ sample/mainAut.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import datetime
+from data import db_session, LoginForm
+from data.users import User
+from flask_login import LoginManager, login_user
+from flask import redirect, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+db_session.global_init("db/blogs.db")
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm.LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+if __name__ == '__main__':
+    app.run()
